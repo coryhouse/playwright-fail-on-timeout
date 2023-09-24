0bb093884c2178140e56c168a399736841f81e6d
Cory House <housecor@gmail.com>
2023-09-24 09:09:12 -0500

    call util

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
-import { throwIfConsole } from "./testUtils";
+import { throwOnConsole } from "./testUtils";
 
 test("should fail the test when an HTTP call times out and provide a helpful error message", async ({
   page,
 }) => {
   await page.goto("http://localhost:3000");
 
-  throwIfConsole(page);
+  throwOnConsole(page);
 
   await expect(
     page.getByRole("heading", { name: "Playwright Timeout Demo" })
