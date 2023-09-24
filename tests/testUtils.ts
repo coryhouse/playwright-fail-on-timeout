c4909a750284534b57a5f227a36bceb8984765e8
Cory House <housecor@gmail.com>
2023-09-24 09:09:01 -0500

    refactor

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,7 +1,7 @@
 import { Page } from "@playwright/test";
 
-// To keep the console clean, fail test immediately if a console message occurs.
-export function throwIfConsole(page: Page) {
+// To keep the console clean, throw an error to fail the test immediately if a console message occurs.
+export function throwOnConsole(page: Page) {
   page.on("console", (message) => {
     // Optionally, can check for only errors like this: if (message.type() === "error") {
     throw new Error(message.text());
